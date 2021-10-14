20d7858b38327621abe9fa67b95d11272de49cb7
1Consumption <hanseop95@gmail.com>
2021-10-15 01:09:47 +0900

    로그인과 presneting을 담당하는 OnboardingViewModel 생성

---
+++ Nanagong/Nanagong/OnboardingViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  OnboardingViewModel.swift
+//  Nanagong
+//
+//  Created by Olaf on 2021/10/15.
+//
+
+import AuthenticationServices
+import Combine
+import Foundation
+import GoogleSignIn
+import KakaoSDKAuth
+
+final class OnboardingViewModel {
+    
+    private let appleSessionManager: AppleSessionMananger
+    private let googleSessionManager: GoogleSessiongManager
+    private let kakaoSessionManager: KakaoSessionManager
+    private let networkManager: NetworkManager
+    
+    init(appleSessionManager: AppleSessionMananger,
+         googleSessionManager: GoogleSessiongManager,
+         kakaoSessionManager: KakaoSessionManager,
+         networkManager: NetworkManager) {
+        self.appleSessionManager = appleSessionManager
+        self.googleSessionManager = googleSessionManager
+        self.kakaoSessionManager = kakaoSessionManager
+        self.networkManager = networkManager
+    }
+    
+    func signInWithApple() -> AnyPublisher<ASAuthorizationAppleIDCredential, Error> {
+        return appleSessionManager.signIn()
+    }
+    
+    func signInWithGoogle(presentViewController: UIViewController) -> AnyPublisher<GIDGoogleUser, GoogleSessionManagerError> {
+        return googleSessionManager.signIn(presentViewController: presentViewController)
+    }
+    
+    func signInWithKakao() -> AnyPublisher<OAuthToken, KakaoSessionManagerError> {
+        return kakaoSessionManager.loginWithKakao()
+    }
+}
