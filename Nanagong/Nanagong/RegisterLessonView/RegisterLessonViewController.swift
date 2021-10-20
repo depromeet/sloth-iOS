b746eb98a49b2142efb078a25c00a3dfd4cbdb8d
1Consumption <hanseop95@gmail.com>
2021-10-20 22:49:22 +0900

    키보드 위치에 따라 button의 위치를 조정함

--- Nanagong/Nanagong/RegisterLessonView/RegisterLessonViewController.swift
+++ Nanagong/Nanagong/RegisterLessonView/RegisterLessonViewController.swift
@@ -34,15 +34,25 @@ final class RegisterLessonViewController: UIViewController {
     }()
     
     private let layoutContainer: RegisterLessonViewLayoutContainer = .init()
+    private var nextButtonleadingConstraint: NSLayoutConstraint!
+    private var nextButtonTrailingConstraint: NSLayoutConstraint!
+    private var nextButtonBottomConstraint: NSLayoutConstraint!
     
     init() {
         super.init(nibName: nil, bundle: nil)
         
         setUpSubviews()
+        addObservers()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        view.endEditing(true)
     }
     
     private func setUpSubviews() {
@@ -51,6 +61,17 @@ final class RegisterLessonViewController: UIViewController {
         setUpProgressView()
         setUpTitleLabel()
         setUpNextButton()
+    }
+    
+    private func addObservers() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(notification:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(notification:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
     }
     
     private func setUpProgressView() {
@@ -83,10 +104,41 @@ final class RegisterLessonViewController: UIViewController {
         
         nextButton.setTitle("다음")
         
+        nextButtonleadingConstraint = nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: layoutContainer.inset.left)
+        nextButtonTrailingConstraint = nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -layoutContainer.inset.right)
+        nextButtonBottomConstraint = nextButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         NSLayoutConstraint.activate([
-            nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: layoutContainer.inset.left),
-            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -layoutContainer.inset.right),
-            nextButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            nextButtonleadingConstraint,
+            nextButtonTrailingConstraint,
+            nextButtonBottomConstraint
         ])
+    }
+    
+    @objc
+    private func keyboardWillShow(notification: NSNotification) {
+        guard let keyboard = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+        }
+        
+        nextButtonBottomConstraint.constant -= (keyboard.height - view.safeAreaInsets.bottom)
+        nextButtonleadingConstraint.constant = 0
+        nextButtonTrailingConstraint.constant = 0
+        nextButton.toggleCornerRadius()
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+    }
+    
+    @objc
+    private func keyboardWillHide(notification: NSNotification) {
+        guard let keyboard = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+        }
+        
+        nextButtonBottomConstraint.constant += (keyboard.height - view.safeAreaInsets.bottom)
+        nextButtonleadingConstraint.constant = layoutContainer.inset.left
+        nextButtonTrailingConstraint.constant = -layoutContainer.inset.right
+        nextButton.toggleCornerRadius()
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
     }
 }
