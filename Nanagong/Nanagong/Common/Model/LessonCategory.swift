21a961a83ea0b3ddfe3c068c98c45a2de08572ec
1Consumption <hanseop95@gmail.com>
2021-10-26 01:48:23 +0900

    LessonCategory 모델 생성

---
+++ Nanagong/Nanagong/Common/Model/LessonCategory.swift
@@ -0,0 +1,14 @@
+//
+//  LessonCategory.swift
+//  Nanagong
+//
+//  Created by Olaf on 2021/10/26.
+//
+
+import Foundation
+
+struct LessonCateogry: Decodable {
+    
+    let categoryId: Int
+    let cateogryName: String
+}
