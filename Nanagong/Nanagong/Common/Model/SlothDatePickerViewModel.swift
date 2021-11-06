dc6c67409eacad922b2a5554b53fe6f9e08e2a73
1Consumption <hanseop95@gmail.com>
2021-11-06 21:55:37 +0900

    bottom sheet에서 datePicker를 선택할 수 있는 SlothDatePickerViewController 생성

---
+++ Nanagong/Nanagong/Common/Model/SlothDatePickerViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SlothDatePickerViewModel.swift
+//  Nanagong
+//
+//  Created by Olaf on 2021/11/06.
+//
+
+import Foundation
+
+final class SlothDatePickerViewModel {
+    
+}
---
+++ Nanagong/Nanagong/Common/View/SlothDatePickerViewController.swift
@@ -0,0 +1,102 @@
+//
+//  SlothDatePickerViewController.swift
+//  Nanagong
+//
+//  Created by Olaf on 2021/11/06.
+//
+
+import SlothDesignSystemModule
+import UIKit
+
+final class SlothDatePickerViewController: UIViewController {
+    
+    private let datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.locale = Locale(identifier: "ko-KR")
+        datePicker.timeZone = .autoupdatingCurrent
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        
+        return datePicker
+    }()
+    
+    private let confirmButton: SlothButton = {
+        let button = SlothButton(buttonStyle: .primary)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("확인")
+        button.changeCornerToRound()
+        
+        return button
+    }()
+    
+    private let viewModel: SlothDatePickerViewModel
+    private let layoutContainer: SlothDatePickerViewLayoutContainer
+    
+    init(viewModel: SlothDatePickerViewModel, layoutContainer: SlothDatePickerViewLayoutContainer) {
+        self.viewModel = viewModel
+        self.layoutContainer = layoutContainer
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        setUpSubviews()
+    }
+    
+    private func setUpSubviews() {
+        setUpDatePicker()
+        setUpConfirmButton()
+    }
+    
+    private func setUpDatePicker() {
+        view.addSubview(datePicker)
+        
+        NSLayoutConstraint.activate([
+            datePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: layoutContainer.datePicker.inset.left),
+            datePicker.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -layoutContainer.datePicker.inset.right),
+            datePicker.topAnchor.constraint(equalTo: view.topAnchor, constant: layoutContainer.datePicker.inset.top),
+            datePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        datePicker.preferredDatePickerStyle = layoutContainer.datePicker.preferredDatePickerStyle
+        datePicker.datePickerMode = layoutContainer.datePicker.datePickerMode
+        datePicker.tintColor = layoutContainer.datePicker.tintColor
+    }
+    
+    private func setUpConfirmButton() {
+        view.addSubview(confirmButton)
+        
+        NSLayoutConstraint.activate([
+            confirmButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: layoutContainer.confirmButton.inset.left),
+            confirmButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -layoutContainer.confirmButton.inset.right),
+            confirmButton.topAnchor.constraint(equalTo: datePicker.bottomAnchor, constant: layoutContainer.confirmButton.inset.top),
+            confirmButton.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor, constant: -layoutContainer.confirmButton.inset.bottom)
+        ])
+    }
+}
+
+extension SlothDatePickerViewController: DimPresentationControllerDelegate {
+    
+    func frameOfPresentedViewInContainerView(frame: CGRect) -> CGRect {
+        let height = datePicker.bounds.height + confirmButton.bounds.height + layoutContainer.heightOfMargins
+        
+        return .init(origin: .init(x: 0, y: frame.height - height),
+                     size: .init(width: frame.width, height: height))
+    }
+}
+
+extension SlothDatePickerViewController: UIViewControllerTransitioningDelegate {
+    
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        return DimPresentationController(presentaionDelegate: self,
+                                         presentedViewController: presented,
+                                         presenting: presenting)
+    }
+}
+
